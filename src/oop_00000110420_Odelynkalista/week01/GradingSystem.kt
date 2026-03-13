acdc41691e9702bbabf8a0e39f8d9c665b3c1d58
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-03-13 11:09:13 +0700

    week01: refactor to use val and string templates

--- src/oop_00000110420_Odelynkalista/week01/GradingSystem.kt
+++ src/oop_00000110420_Odelynkalista/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000110420_Odelynkalista.week01
 
 fun main() {
-    var name: String = "John thor"
-    var score: Int = 80
+    val name: String = "John thor"
+    val score: Int = 80
 
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: " $name + ", Nilai: " $score)
 }
