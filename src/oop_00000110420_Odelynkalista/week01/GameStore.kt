e9c73c092a44c5c39eae39aa1a2b44f8f2256dde
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-03-27 10:25:48 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000110420_Odelynkalista/week01/GameStore.kt
+++ src/oop_00000110420_Odelynkalista/week01/GameStore.kt
@@ -5,8 +5,18 @@ fun main() {
     val price = 600000
 
     val finalPrice = calculateDiscount(price)
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
 }
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) (price * 80) / 100
     else (price * 90) / 100
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Title: $title")
+    println("Final Price: $finalPrice")
+}
