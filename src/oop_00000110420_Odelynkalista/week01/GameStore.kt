1bf3f5a3aa848aaa689b4ff370d17b7faab34307
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-03-27 10:25:20 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000110420_Odelynkalista/week01/GameStore.kt
+++ src/oop_00000110420_Odelynkalista/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_00000110420_Odelynkalista.week01
 fun main() {
     val gameTitle = "Game A"
     val price = 600000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 80) / 100
+    else (price * 90) / 100
