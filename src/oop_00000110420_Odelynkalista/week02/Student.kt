33749dce54924a0d4f963df13a067a4e01d0b712
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-03-27 11:08:23 +0700

    week02: add warning validation in init block

--- src/oop_00000110420_Odelynkalista/week02/Student.kt
+++ src/oop_00000110420_Odelynkalista/week02/Student.kt
@@ -4,6 +4,13 @@ class Student(
     val name: String,
     val nim: String,
     var major: String
-){
-
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek student $name berhasil dialokasikan di memory.")
+        }
+    }
 }
