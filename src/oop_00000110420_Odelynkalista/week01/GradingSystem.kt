1926a0acee963476a1539a1a12cc34340fcc52ae
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-03-27 08:38:32 +0700

    week01: implement grading logic using when expression

--- src/oop_00000110420_Odelynkalista/week01/GradingSystem.kt
+++ src/oop_00000110420_Odelynkalista/week01/GradingSystem.kt
@@ -4,5 +4,14 @@ fun main() {
     val name: String = "John thor"
     val score: Int = 80
 
-    println("Nama: " $name + ", Nilai: " $score)
+    println("Grade kamu: $grade")
+
+    val grade = when (score) {
+        in 90 .. 100 -> "A"
+        in 80 .. 89 -> "B"
+        in 70 .. 79 -> "C"
+        else -> "D"
+    }
+
+    println("Grade kamu: $grade")
 }
