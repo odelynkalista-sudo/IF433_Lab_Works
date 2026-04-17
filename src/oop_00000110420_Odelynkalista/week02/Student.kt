5e8b22f504d6a9d52b2fe33ad6031afb79896960
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-17 10:53:20 +0700

    week02: add secondary constructor

--- src/oop_00000110420_Odelynkalista/week02/Student.kt
+++ src/oop_00000110420_Odelynkalista/week02/Student.kt
@@ -3,15 +3,18 @@ package oop_00000110420_Odelynkalista.week02
 class Student (
     val name: String,
     val nim: String,
-    val major: String
-){
-
+    var major: String = "Non-Matriculated",
+    var gpa: Double = 0.0
+) {
     init {
-        if (nim.length != 5){
+        if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
+    }
+
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated", 0.0) {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
