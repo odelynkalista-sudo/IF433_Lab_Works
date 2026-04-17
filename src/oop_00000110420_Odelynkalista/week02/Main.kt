9952eab87549275232779054e244fbc10c0b3ddd
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-17 10:59:08 +0700

    week02: implement selection logic for constructors

--- src/oop_00000110420_Odelynkalista/week02/Main.kt
+++ src/oop_00000110420_Odelynkalista/week02/Main.kt
@@ -18,10 +18,22 @@ fun main () {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
 
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
+
         println("Status: Pendaftaran Selesai.")
     }
 }
