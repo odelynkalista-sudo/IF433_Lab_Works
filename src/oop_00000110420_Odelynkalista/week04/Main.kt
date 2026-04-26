6d126f0f1e57a203eb84cf57ad657d575a58b9c5
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 04:01:35 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000110420_Odelynkalista/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000110420_Odelynkalista.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
