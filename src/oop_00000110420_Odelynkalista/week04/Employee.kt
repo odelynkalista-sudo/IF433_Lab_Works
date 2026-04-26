31b52c14ba52ef0d2cdb422d7f03b5f9c4123a0c
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 04:14:14 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_00000110420_Odelynkalista/week04/Employee.kt
@@ -0,0 +1,12 @@
+package oop_00000110420_Odelynkalista.week04
+
+open class Employee(val name: String, val baseSalary: Int) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt()
+    }
+}
