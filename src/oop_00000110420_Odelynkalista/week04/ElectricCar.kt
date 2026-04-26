b709f75488c7ff3913866db8bf885c91952d5a46
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 04:11:24 +0700

    week04: (task 1) create ElectricCar class with final override

--- src/oop_00000110420_Odelynkalista/week04/ElectricCar.kt
+++ src/oop_00000110420_Odelynkalista/week04/ElectricCar.kt
@@ -1,2 +1,9 @@
 package oop_00000110420_Odelynkalista.week04
 
+class ElectricCar(brand: String, numberOfDoors: Int, val batteryCapacity: Int)
+    : Car(brand, numberOfDoors) { // Constructor chaining ke Car [cite: 67, 68]
+
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+}
--- src/oop_00000110420_Odelynkalista/week04/car.kt
+++ src/oop_00000110420_Odelynkalista/week04/car.kt
@@ -1,6 +1,6 @@
 package oop_00000110420_Odelynkalista.week04
 
-open class car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
