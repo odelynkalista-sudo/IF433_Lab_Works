398af46ebc460545f13651f8619963abecae4a29
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 02:53:57 +0700

    week03: fix setter using backing field

--- src/oop_00000110420_Odelynkalista/week03/Employee.kt
+++ src/oop_00000110420_Odelynkalista/week03/Employee.kt
@@ -2,4 +2,13 @@ package oop_00000110420_Odelynkalista.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0){
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
+
+        }
 }
--- src/oop_00000110420_Odelynkalista/week03/Main.kt
+++ src/oop_00000110420_Odelynkalista/week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_00000110420_Odelynkalista.week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
