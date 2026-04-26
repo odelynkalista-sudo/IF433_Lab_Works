eef18893bc27bbdae54c27742bc0a9d927e08343
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 04:16:58 +0700

    week04: (task 2) create Manager class overriding Employee

--- src/oop_00000110420_Odelynkalista/week04/Manager.kt
+++ src/oop_00000110420_Odelynkalista/week04/Manager.kt
@@ -1,7 +1,7 @@
 package oop_00000110420_Odelynkalista.week04
 
 class Manager (name: String, baseSalary: Int) : Employee(name, baseSalary) {
-        [cite: 86]
+
 
         override fun work() {
             println("$name sedang memimpin rapat divisi.")
