6815aac58203113158b1703f3a135eebf38a3856
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 03:23:48 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000110420_Odelynkalista/week03/Main.kt
+++ src/oop_00000110420_Odelynkalista/week03/Main.kt
@@ -3,11 +3,17 @@ package oop_00000110420_Odelynkalista.week03
 fun main() {
     val e = Employee("Budi")
 
-    e.salary= -1000
-    e.salary= 5000000
+    e.salary = -1000
+    e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     e.increasePerformance()
 
-    println("Pajak yang harus di bayar: ${e.tax}")
+    println("\n--- Test Tugas 1: Weapon ---")
+    val sword = Weapon("Excalibur")
+
+    sword.damage = -50
+    sword.damage = 9999
+    println("Weapon: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
 }
+
