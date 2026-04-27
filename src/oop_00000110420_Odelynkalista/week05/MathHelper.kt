4cc122e63de1d36a29dba5dc3ace2c30f0041d96
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 15:24:21 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000110420_Odelynkalista/week05/MathHelper.kt
@@ -0,0 +1,14 @@
+package oop_00000110420_Odelynkalista.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
