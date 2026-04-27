5b4d436a49da1c75b2f3ca5067d9f9f82b248237
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-27 15:18:52 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000110420_Odelynkalista/week05/Main.kt
@@ -0,0 +1,25 @@
+package oop_00000110420_Odelynkalista.week05
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex", nidn = "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIFITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------")
+    }
+}
