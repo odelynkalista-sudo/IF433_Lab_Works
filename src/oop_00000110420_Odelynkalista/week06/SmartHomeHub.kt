88069b2db4994197330ac2bb2e7bd2445984a59a
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-29 20:02:27 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000110420_Odelynkalista/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000110420_Odelynkalista.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
