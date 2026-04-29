c050e49efa64f8b5e5011e265d6b6e97f43039fb
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-30 03:03:35 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_00000110420_Odelynkalista/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_00000110420_Odelynkalista.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
