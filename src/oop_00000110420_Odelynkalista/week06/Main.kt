a32fd17b76fa2498c958cb5fa0b5790bb20db0ab
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-29 20:08:21 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000110420_Odelynkalista/week06/Main.kt
+++ src/oop_00000110420_Odelynkalista/week06/Main.kt
@@ -21,4 +21,15 @@ fun main() {
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n--- Mengaktifkan Mode Keamanan ---")
+    hub.activateSecurityMode()
+
+    println("\n--- Mematikan Semua Saklar ---")
+    hub.turnOffAllSwitches()
 }
