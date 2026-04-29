064c6e5e78d8dfd9392783b5bb67bb4568f03103
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-29 19:40:38 +0700

    week06: create Gopay and CreditCard

--- src/oop_00000110420_Odelynkalista/week06/PaymentMethod.kt
+++ src/oop_00000110420_Odelynkalista/week06/PaymentMethod.kt
@@ -3,3 +3,11 @@ package oop_00000110420_Odelynkalista.week06
 interface PaymentMethod {
     fun pay(amount: Double)
 }
+
+class Gopay: PaymentMethod {
+    override fun pay(amount: Double) { println("Processing Rp$amount via Gopay Server") }
+}
+
+class CreditCard: PaymentMethod {
+    override fun pay(amount: Double) { println("Contracting Bank for Rp$amount") }
+}
