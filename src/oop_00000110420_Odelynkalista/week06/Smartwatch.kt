45ddb890d7547d207cccccb9d70b784ca2188ae7
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-29 19:27:43 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000110420_Odelynkalista/week06/Smartwatch.kt
@@ -0,0 +1,14 @@
+package oop_00000110420_Odelynkalista.week06
+
+class Smartwatch : Watch(), BlutoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBlutooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing")
+    }
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W")
+    }
+}
