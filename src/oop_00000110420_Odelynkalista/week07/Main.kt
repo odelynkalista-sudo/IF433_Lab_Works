05ec567676d41ea80d040271393ca10435bbf9d6
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-30 02:55:05 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000110420_Odelynkalista/week07/Main.kt
+++ src/oop_00000110420_Odelynkalista/week07/Main.kt
@@ -31,7 +31,8 @@ fun main() {
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
     val uiMessage = when(response) {
-        is ApiResponse.Success -> "Tampilkan${response.data}"
+        is ApiResponse.Success -> "Tampilkan ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
 }
