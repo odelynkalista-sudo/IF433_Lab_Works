6b37625044c5ab20951f02eb3f8fe7bb21397387
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-30 13:46:54 +0700

    week08: (task) implement payment processing with git add .

--- src/oop_00000110420_Odelynkalista/week08/ApiParser.kt
+++ src/oop_00000110420_Odelynkalista/week08/ApiParser.kt
@@ -19,4 +19,13 @@ class ApiParser {
            else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+        val transactionId = JavaPaymentService.processPayment(id)!!
+        println("Checkout Berhasil! ID Transaksi: $transactionId")
+    }
 }
