616fec967cad54a754ac217319c6d0ab70551d84
Asang Aho <asang@Odelyns-MacBook-Air.local>
2026-04-30 13:50:31 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/oop_00000110420_Odelynkalista/week08/ECommerceMain.kt
@@ -0,0 +1,28 @@
+package oop_00000110420_Odelynkalista.week08
+
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Waranty korup
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // ID Hilang!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Tipe tidak dikenal
+    )
+
+    val parser = ApiParser()
+
+    println("=== STARTING E-COMMERCE PIPELINE ===")
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let {
+                println("Berhasil Parsing: ${it.name}")
+            } ?: println("Log: Produk di-skip karena tipe tidak dikenal.")
+
+        } catch (e: IllegalArgumentException) {
+            println("DATA ERROR: ${e.message}")
+        }
+        println("-------------------")
+    }
+}
